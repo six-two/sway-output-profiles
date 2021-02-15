e83af6ebd24693d0e4f5c6a072e3cb1d6da73bcf
six-two <git@six-two.dev>
2021-02-15 16:29:16 +0100

    Implemented command 'toggle'

--- sway-output-profiles.py
+++ sway-output-profiles.py
@@ -76,6 +76,19 @@ def wrong_usage():
     sys.exit(1)
 
 
+def apply_profile(profile_name: str) -> None:
+    for profile in config["profiles"]:
+        # Find the selected profile
+        if profile["name"] == profile_name:
+            for command in profile["commands"]:
+                subprocess.call([command], shell=True)
+            return
+
+    print(f"Error: Unknown profile '{profile_name}'!\n")
+    subcommand_list(config)
+    sys.exit(1)
+
+
 def subcommand_list(config: dict) -> None:
     names = [p.get("name") for p in config["profiles"]]
     print(f"The following {len(names)} profile(s) can be used:")
@@ -89,16 +102,33 @@ def subcommand_get(config: dict) -> None:
 
 
 def subcommand_set(config: dict, profile_name: str) -> None:
-    for profile in config["profiles"]:
-        # Find the selected profile
-        if profile["name"] == profile_name:
-            write_state(config, profile_name)
-            for command in profile["commands"]:
-                subprocess.call([command], shell=True)
-            return
+    apply_profile(profile_name)
+    write_state(config, profile_name)
 
-    print(f"Error: Unknown profile '{profile_name}'!\n")
-    subcommand_list(config)
+
+def subcommand_toggle(config: dict, profile_1: str, profile_2: str = "") -> None:
+    state = read_state(config)
+    parsed_state = state.split("\n")
+    current = parsed_state[0]
+
+    if profile_1 != current:
+        # Apply profile 1
+        apply_profile(profile_1)
+        write_state(config, f"{profile_1}\n{current}")
+    else:
+        # If necessary look up profile 2 from state
+        if not profile_2 and len(parsed_state) > 1:
+            profile_2 = parsed_state[1]
+
+        if profile_2:
+            # Apply profile 2
+            apply_profile(profile_2)
+            write_state(config, f"{profile_2}\n{current}")
+        else:
+            # Toggling to active profile makes no sense
+            # so we just do exactly nothing
+            pass
+
 
 
 if __name__ == "__main__":
@@ -127,9 +157,9 @@ if __name__ == "__main__":
             wrong_usage()
     elif subcommand == "toggle":
         if len(args) == 2:
-            pass
+            subcommand_toggle(config, args[1])
         elif len(args) == 3:
-            pass
+            subcommand_toggle(config, args[1], args[2])
         else:
             wrong_usage()
     else:
